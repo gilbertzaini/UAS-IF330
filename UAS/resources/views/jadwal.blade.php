c3469ddbb1a1fd50529e3a9acd3bb0434db4de41
gilbertzaini <gilbertzaini03@gmail.com>
2023-06-01 16:06:15 +0700

    add jadwal page

---
+++ UAS/resources/views/jadwal.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.master')
+
+@section('content')
+<ul id="listJadwal">
+    @for ($i = 0; $i < 5; $i++)        
+    <li class="dokterJadwal">
+        <div class="col-5 mx-auto my-2">
+            <div class="d-flex d align-items-center justify-content-between">
+                <div class="d-flex justify-content-start mt-2">
+                    <div class="d-flex flex-column justify-content-center align-items-center"/>
+                        <img class="jadwalFoto mx-4" src="{{asset('storage/assets/fotoDokter/bocil.jpg')}}"/>
+                    </div>
+                    <div class="d-flex align-items-center" style="word-wrap: break-word;">
+                        <div class="d-flex flex-column align-items-center justify-content-center col-8">
+                            <a href="#" class="namaDokter text-start" style="word-wrap: break-word; font-weight:bold;">Hj. dr. Indah Desri Wahyuni, Sp.A, M.Kes</a>
+                        </div>
+                        <img class="ml-3" style="width: 4.5em; padding-left: 1em;" src="{{asset('storage/assets/eyeLogo.png')}}"/>
+                    </div>
+                </div>                
+                
+                <div class="d-flex justify-content-start px-2 jadwalDokter mt-2 col-4">
+                    <ul>
+                        <li>Senin 08.00-12.00</li>
+                        <li>Selasa 08.00 - 12.00</li>
+                        <li>Rabu 08.00 - 10.00</li>                        
+                        <li>Kamis 08.00 - 09.00</li>                        
+                        <li>Jumat 08.00 - 15.00</li>
+                    </ul>
+                </div>
+            </div>   
+            <div>
+                <a class="btn btn-primary mt-2" style="width:100%;">Buat Jadwal</a>
+            </div>          
+        </div>
+    </li>
+    @endfor
+</ul>
+@stop
