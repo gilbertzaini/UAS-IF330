9ae28b1e44be2b937b7abe6284b6fb28b37e8e3b
gilbertzaini <gilbertzaini03@gmail.com>
2023-06-08 18:47:41 +0700

    updated review table

--- UAS/database/migrations/2023_06_07_102510_create_review_table.php
+++ UAS/database/migrations/2023_06_07_102510_create_review_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->string('doctor_id');
+            $table->string('user_id');            
+            $table->integer('bintang');
+            $table->string('ulasan');
             $table->timestamps();
         });
     }
