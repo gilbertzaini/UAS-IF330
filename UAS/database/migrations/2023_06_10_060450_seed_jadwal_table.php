251e51204b08ed8f8164892c3345a1124b326353
gilbertzaini <gilbertzaini03@gmail.com>
2023-06-11 18:27:00 +0700

    Update 2023_06_10_060450_seed_jadwal_table.php

--- UAS/database/migrations/2023_06_10_060450_seed_jadwal_table.php
+++ UAS/database/migrations/2023_06_10_060450_seed_jadwal_table.php
@@ -13,167 +13,167 @@ return new class extends Migration
     {
         DB::table('jadwals')->insert([
             [
-                'idDokter' => '1',
+                'doctor_id' => '1',
                 'jadwalPraktik' => 'Senin 08.00-12.00'
             ],
             [
-                'idDokter' => '1',
+                'doctor_id' => '1',
                 'jadwalPraktik' => 'Selasa 08.00 - 12.00'
             ],
             [
-                'idDokter' => '1',
+                'doctor_id' => '1',
                 'jadwalPraktik' => 'Rabu 08.00 - 10.00'
             ],
             [
-                'idDokter' => '1',
+                'doctor_id' => '1',
                 'jadwalPraktik' => 'Kamis 08.00 - 09.00'
             ],
             [
-                'idDokter' => '1',
+                'doctor_id' => '1',
                 'jadwalPraktik' => 'Jumat 08.00 - 15.00'
             ],
             [
-                'idDokter' => '2',
+                'doctor_id' => '2',
                 'jadwalPraktik' => 'Selasa 10.00 - 12.00'
             ],
             [
-                'idDokter' => '2',
+                'doctor_id' => '2',
                 'jadwalPraktik' => 'Kamis 10.00-12.00'
             ],
             [
-                'idDokter' => '3',
+                'doctor_id' => '3',
                 'jadwalPraktik' => 'Selasa 12.00 - 14.00'
             ],
             [
-                'idDokter' => '3',
+                'doctor_id' => '3',
                 'jadwalPraktik' => 'Kamis 09.00 - 12.00'
             ],
             [
-                'idDokter' => '3',
+                'doctor_id' => '3',
                 'jadwalPraktik' => 'Sabtu 09.00 - 12.00'
             ],
             [
-                'idDokter' => '4',
+                'doctor_id' => '4',
                 'jadwalPraktik' => 'Senin 16.00 - 20.00'
             ],
             [
-                'idDokter' => '4',
+                'doctor_id' => '4',
                 'jadwalPraktik' => 'Rabu 16.00 - 20.00'
             ],
             [
-                'idDokter' => '5',
+                'doctor_id' => '5',
                 'jadwalPraktik' => 'Senin 15.30 - 19.00'
             ],
             [
-                'idDokter' => '5',
+                'doctor_id' => '5',
                 'jadwalPraktik' => 'Selasa 09.30 - 15.00'
             ],
             [
-                'idDokter' => '5',
+                'doctor_id' => '5',
                 'jadwalPraktik' => 'Rabu 09.30 - 15.00'
             ],
             [
-                'idDokter' => '5',
+                'doctor_id' => '5',
                 'jadwalPraktik' => 'Kamis 15.30 - 18.00'
             ],
             [
-                'idDokter' => '6',
+                'doctor_id' => '6',
                 'jadwalPraktik' => 'Jumat 16.00 - 19.00'
             ],
             [
-                'idDokter' => '7',
+                'doctor_id' => '7',
                 'jadwalPraktik' => 'Senin 10.00 - 14.00'
             ],
             [
-                'idDokter' => '7',
+                'doctor_id' => '7',
                 'jadwalPraktik' => 'Rabu 10.00 - 14.00'
             ],
             [
-                'idDokter' => '7',
+                'doctor_id' => '7',
                 'jadwalPraktik' => 'Sabtu 15.00 - 18.00'
             ],
             [
-                'idDokter' => '8',
+                'doctor_id' => '8',
                 'jadwalPraktik' => 'Senin.13.00 - 16.00'
             ],
             [
-                'idDokter' => '8',
+                'doctor_id' => '8',
                 'jadwalPraktik' => 'Rabu 13.00 - 18.00'
             ],
             [
-                'idDokter' => '8',
+                'doctor_id' => '8',
                 'jadwalPraktik' => 'Kamis 13.00 - 15.00'
             ],
             [
-                'idDokter' => '8',
+                'doctor_id' => '8',
                 'jadwalPraktik' => 'Jumat 09.00 - 12.00'
             ],
             [
-                'idDokter' => '9',
+                'doctor_id' => '9',
                 'jadwalPraktik' => 'Senin 16.00 - 18.00'
             ],
             [
-                'idDokter' => '9',
+                'doctor_id' => '9',
                 'jadwalPraktik' => 'Selasa 16.00 - 18.00'
             ],
             [
-                'idDokter' => '9',
+                'doctor_id' => '9',
                 'jadwalPraktik' => 'Rabu 18.00-.20.00'
             ],
             [
-                'idDokter' => '10',
+                'doctor_id' => '10',
                 'jadwalPraktik' => 'Selasa 15.00 - 17.00'
             ],
             [
-                'idDokter' => '10',
+                'doctor_id' => '10',
                 'jadwalPraktik' => 'Kamis 15.00 - 17.00'
             ],
             [
-                'idDokter' => '10',
+                'doctor_id' => '10',
                 'jadwalPraktik' => 'Sabtu 12.00 - 14.00'
             ],
             [
-                'idDokter' => '11',
+                'doctor_id' => '11',
                 'jadwalPraktik' => 'Senin 15.30 - 19.00'
             ],
             [
-                'idDokter' => '11',
+                'doctor_id' => '11',
                 'jadwalPraktik' => 'Selasa 09.30 - 15.00'
             ],
             [
-                'idDokter' => '11',
+                'doctor_id' => '11',
                 'jadwalPraktik' => 'Jumat 09.30 - 15.00'
             ],
             [
-                'idDokter' => '12',
+                'doctor_id' => '12',
                 'jadwalPraktik' => 'Jumat 16.00 - 19.00'
             ],
             [
-                'idDokter' => '12',
+                'doctor_id' => '12',
                 'jadwalPraktik' => 'Sabtu 8.00 - 10.00'
             ],
             [
-                'idDokter' => '13',
+                'doctor_id' => '13',
                 'jadwalPraktik' => 'Senin 13.00-15.00'
             ],
             [
-                'idDokter' => '13',
+                'doctor_id' => '13',
                 'jadwalPraktik' => 'Selasa 13.00-15.00'
             ],
             [
-                'idDokter' => '13',
+                'doctor_id' => '13',
                 'jadwalPraktik' => 'Rabu 13.00-15.00'
             ],
             [
-                'idDokter' => '14',
+                'doctor_id' => '14',
                 'jadwalPraktik' => 'Senin 16.00-18.00'
             ],
             [
-                'idDokter' => '14',
+                'doctor_id' => '14',
                 'jadwalPraktik' => 'Kamis 16.00-18.00'
             ],
             [
-                'idDokter' => '14',
+                'doctor_id' => '14',
                 'jadwalPraktik' => 'Sabtu 15.00-17.00'
             ]
             
